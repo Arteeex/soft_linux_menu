b93f6995fe72ee7a413cff4fb131a8194a41ef25
arteeex <arteeex187@gmail.com>
2025-02-12 23:19:14 +0700

    version 1.1.0

--- menu.sh
+++ menu.sh
@@ -2,44 +2,52 @@
 
 #read-menu: Программа вывода системной информации, управляемая с помощью меню
 
+DELAY=4 # Время отображения результатов на экране (в секундах)
+
+
+while true; do
 clear
-echo "
-Пожалуйста выберите: 
+cat <<- _EOF_ 
+ Пожалуйста выберите: 
 
 1. Системная информация
 2. Свободное место
 3. Температура
 0. Выход
-"
+
+_EOF_
 read -p "Выберите цифру [0-3] > "
 
 if [[ "$REPLY" =~ ^[0-3]$ ]]; then
-	if [[ "$REPLY" == 0 ]]; then
-	echo "Программа завершила работу."
-	exit
-        fi
-
 	if [[ "$REPLY" == 1 ]]; then
-	echo "Hostname: $HOSTNAME"
-	uptime
-	exit
+		echo "Hostname: $HOSTNAME"
+		uptime
+		sleep "$DELAY"
+		continue
 	fi
 
 	if [[ "$REPLY" == 2 ]]; then
-	echo "Свободное место."
-	df -h
-	exit
+		echo "Свободное место."
+		df -h
+		sleep "$DELAY"
+		continue
 	fi
 	
 	if [[ "$REPLY" == 3 ]]; then
-	watch -d -n 2 sensors
-	exit
+		watch -d -n 2 sensors
+		sleep 1
+		continue
+	fi
+	if [[ "$REPLY" == 0 ]]; then
+		break
 	fi
 else
-	echo "Программа завершилось с ошибкой" >&2
-	exit 1
-	fi
+	echo "Ошибка в программе."
+	sleep $DELAY
+fi
 
+done
+echo "Программа завершилась."
 
 
 
